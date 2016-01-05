2a531d5403a266659264cf90c7735cf5f8a61846
zhoufeng <zhoufengloop@gmail.com>
2016-01-05 22:18:14 +0800

    commit.

--- src/main/scala/com/github/zhoufeng1989/tldr/Tldr.scala
+++ src/main/scala/com/github/zhoufeng1989/tldr/Tldr.scala
@@ -8,28 +8,63 @@ import scala.collection.JavaConverters._
 import sys.process._
 import scala.util.{Try, Success, Failure}
 
+sealed abstract class Command {
+  def exec: Unit
+}
+case class Find(command: String, platforms: List[String], pagesDir: String) extends Command {
+  override def exec() = ""
+}
+//case class Update(destRepo: String) extends Command
+//case object Init extends Command
+//case object Help extends Command
+//case object WrongCommand extends Command
+object Command {
+  //def apply(args: Array[String]) = args match {
+  //  case Array("init") => Init
+  //  case Array("help") => Help
+  //  case Array("update") => {
+  //    val env = getEnv()
+  //    Update(env("pagesDir"))
+  //  }
+  //  case Array("find", c) => {
+  //    val env = getEnv()
+  //    Find(c, env("platforms"), env("pagesDir"))
+  //  }
+  //  case _ => WrongCommand
+  //}
+
+  def getEnv() = {
+    val platformMap = Map("OSX" -> "osx")
+    val configFile = s"${System.getProperty("user.home")}/.tldrcc"
+
+    val pagesDir, rootDir = Try{
+      val yaml = new Yaml()
+      val content = yaml.load(scala.io.Source.fromFile(configFile).reader())
+      content.asInstanceOf[java.util.Map[String, String]].asScala.toMap
+    }.map {
+      config => (config("pages"), config("root"))
+    } match {
+      case Failure(_) => throw new RuntimeException("Run tldr init first")
+      case Success((p, r)) => (p, r)
+    }
+
+    val platforms = platformMap get (System.getProperty("os.name")) match {
+      case Some(platform) => List("common", platform)
+      case None => List("common")
+    }
+    val separator = System.getProperty("file.separator")
+    //val pagePaths = platforms map {
+    //  platform => List(pagesDir, platform, command).mkString(System.getProperty("file.separator")) + ".md"
+    //}
+    Map("platforms" -> platforms, "pagesDir" -> pagesDir, "rootDir" -> rootDir)
+  }
+}
+
+
 object Tldr extends App {
-  val platformMap = Map("Mac OS X" -> "osx")
-  val configFile = s"${System.getProperty("user.home")}/.tldrcc"
-  val pagesDir = Try{
-    new Yaml().load(scala.io.Source.fromFile(configFile).reader()).asInstanceOf[java.util.Map[String, String]].asScala.toMap
-  }.map {
-    config => config("pages")
-  } match {
-    case Failure(_) => println("tldr init first"); sys.exit(1)
-    case Success(dir) => dir
-  }
-  val platforms = platformMap get (System.getProperty("os.name")) match {
-    case Some(platform) => List("common", platform)
-    case None => List("common")
-  }
-  val command = "ls"
-  val separator = System.getProperty("file.separator")
-  val pagePaths = platforms map {
-    platform => List(pagesDir, platform, command).mkString(System.getProperty("file.separator")) + ".md"
-  }
-  println(pagePaths)
-  println(pagePaths.map(parse))
+  println(Command.getEnv())
+  //println(pagePaths)
+  //println(pagePaths.map(parse))
   def init(dest: String) = {
     s"git clone https://github.com/tldr-pages/tldr.git ${dest}" !
   }
@@ -37,6 +72,11 @@ object Tldr extends App {
   def update(dest: String) = {
     s"cd ${dest}; git pull" !
   }
+
+  def help():String = {
+    "this is help document"
+  }
+
 
   def parse(pagePath: String): Option[String] = {
     try {
